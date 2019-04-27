e12e2a6215545596600bec0655717769a37beec8
seijikato <katokonn@gmail.com>
2019-04-27 15:35:21 +0900

    UserGetQueryだけ実装した

--- src/main/scala/shiftkun/api/UserApi.scala
+++ src/main/scala/shiftkun/api/UserApi.scala
@@ -6,13 +6,19 @@ import shiftkun.api.common.ApiSupport
 import shiftkun.lib.auth.AuthContext
 import akka.http.scaladsl.server.Route
 import cats.instances.future._
+import shiftkun.api.UserApi.{UserGetOutput, UserQueryResult}
+import shiftkun.domain.model.{LineUserId, User, UserId, UserStatus}
+import shiftkun.lib.ProcessResult
+
+
+import io.circe.generic.auto._
 
 import scala.concurrent.ExecutionContext
 
 class UserApi(val module: AppServiceModule)(implicit ec: ExecutionContext)
   extends ApiSupport
     with ApiErrorMapper {
-  def routes(implicit auth: AuthContext): Route = registerUser
+  def routes(implicit auth: AuthContext): Route = registerUser ~ getUser
 
   import module._
 
@@ -29,4 +35,40 @@ class UserApi(val module: AppServiceModule)(implicit ec: ExecutionContext)
       }
     }
   }
+
+  def getUser(implicit auth: AuthContext): Route = {
+    path("user") {
+      get {
+        val result = ProcessResult.success {
+          UserGetOutput(
+            UserQueryResult(
+              id = 0L,
+              lineUserId = "tempId",
+              name = "sampleくん",
+              picture = "http:sssssssssssssssssiconicon",
+              status = "active"
+            )
+          )
+        }
+        response(result) { r =>
+          complete(r)
+        }
+      }
+    }
+  }
+}
+
+object UserApi {
+
+  case class UserGetOutput(
+    user: UserQueryResult
+  )
+
+  case class UserQueryResult(
+    id: Long,
+    lineUserId: String,
+    name: String,
+    picture: String,
+    status: String,
+  )
 }
